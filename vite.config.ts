059e4b89025c6d5aeaf460b328438ee59ec4e3f9
mramirahmadi2 <amirahmadim24@gmail.com>
2025-04-29 10:20:51 +0330

    fix bug diploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
 })
